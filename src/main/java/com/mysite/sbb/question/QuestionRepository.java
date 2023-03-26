7e4aeed729d16329327672b63d2be14d219abb54
YunJiW <dtroy371@gmail.com>
2023-03-26 18:19:09 +0900

    검색기능추가

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.question;
 import com.mysite.sbb.question.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,6 +18,8 @@ public interface QuestionRepository extends JpaRepository<Question,Integer> {
     List<Question> findBySubjectLike(String subject);
 
     Page<Question> findAll(Pageable pageable);
+
+    Page<Question> findAll(Specification<Question> spec,Pageable pageable);
     @Transactional
     @Modifying //쿼리가 select가 아니라면 modify를 붙여야한다.
     @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
